509c8f9ebcb8ad312a83016d8182f4fc0ba2827d
fadedreams7 <fadedreams7@gmail.com>
2023-11-06 04:50:05 -0500

    tokenverify_fixed

--- accounts/views.py
+++ accounts/views.py
@@ -9,6 +9,11 @@ from .models import User
 from .utils import Util
 from django.contrib.sites.shortcuts import get_current_site
 from django.urls import reverse
+import jwt
+from django.conf import settings
+
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 class RegisterView(generics.GenericAPIView):
 
@@ -22,11 +27,16 @@ class RegisterView(generics.GenericAPIView):
         serializer.save()
         user_data = serializer.data
         user = User.objects.get(email=user_data['email'])
-        token = RefreshToken.for_user(user).access_token
-        print(token)
+        # print("aaaaaaaaaaaaa", user.id)
+        payload = {
+            'user_id': user.id,
+        }
+        access_token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
+        # token = RefreshToken.for_user(user.id).access_token
+        user_data['token'] = access_token
         current_site = get_current_site(request).domain
         relativeLink = reverse('email-verify')
-        absurl = 'http://'+current_site+relativeLink+"?token="+str(token)
+        absurl = 'http://'+current_site+relativeLink+"?token="+str(access_token)
         email_body = 'Hi '+user.username + \
             ' Use the link below to verify your email \n' + absurl
         data = {'email_body': email_body, 'to_email': user.email,
@@ -37,23 +47,29 @@ class RegisterView(generics.GenericAPIView):
 
 
 class VerifyEmail(views.APIView):
-    ...
-    # serializer_class = EmailVerificationSerializer
+    serializer_class = EmailVerificationSerializer
 
-    # token_param_config = openapi.Parameter(
-        # 'token', in_=openapi.IN_QUERY, description='Description', type=openapi.TYPE_STRING)
+    token_param_config = openapi.Parameter(
+        'token', in_=openapi.IN_QUERY, description='Description', type=openapi.TYPE_STRING)
 
-    # @swagger_auto_schema(manual_parameters=[token_param_config])
-    # def get(self, request):
-        # token = request.GET.get('token')
-        # try:
+    @swagger_auto_schema(manual_parameters=[token_param_config])
+    def get(self, request):
+        token = request.GET.get('token')
+        try:
             # payload = jwt.decode(token, settings.SECRET_KEY)
+            payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
+            user_id = payload.get('user_id')
+            print("in try")
+            print(payload)
             # user = User.objects.get(id=payload['user_id'])
-            # if not user.is_verified:
-                # user.is_verified = True
-                # user.save()
-            # return Response({'email': 'Successfully activated'}, status=status.HTTP_200_OK)
-        # except jwt.ExpiredSignatureError as identifier:
-            # return Response({'error': 'Activation Expired'}, status=status.HTTP_400_BAD_REQUEST)
-        # except jwt.exceptions.DecodeError as identifier:
-            # return Response({'error': 'Invalid token'}, status=status.HTTP_400_BAD_REQUEST)
+            user = User.objects.get(id=user_id)
+            if not user.is_verified:
+                user.is_verified = True
+                user.save()
+            return Response({'email': 'Successfully activated'}, status=status.HTTP_200_OK)
+        except jwt.ExpiredSignatureError as identifier:
+            return Response({'error': 'Activation Expired'}, status=status.HTTP_400_BAD_REQUEST)
+        except jwt.exceptions.DecodeError as identifier:
+            return Response({'error': 'Invalid token'}, status=status.HTTP_400_BAD_REQUEST)
+
+
